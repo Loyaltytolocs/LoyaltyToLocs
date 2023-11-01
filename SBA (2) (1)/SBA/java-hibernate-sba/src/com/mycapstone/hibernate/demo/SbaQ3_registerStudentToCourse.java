07960dbc48ac630834ae0e514ec08140ff91664a
Loyaltytolocs <113317913+Loyaltytolocs@users.noreply.github.com>
2023-11-01 15:20:28 -0700

    Add files via upload

---
+++ SBA (2) (1)/SBA/java-hibernate-sba/src/com/mycapstone/hibernate/demo/SbaQ3_registerStudentToCourse.java
@@ -0,0 +1,76 @@
+package com.mycapstone.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mycapstone.hibernate.demo.entity.Course;
+import com.mycapstone.hibernate.demo.entity.Instructor;
+import com.mycapstone.hibernate.demo.entity.InstructorDetail;
+import com.mycapstone.hibernate.demo.entity.Review;
+import com.mycapstone.hibernate.demo.entity.Student;
+
+public class SbaQ3_registerStudentToCourse {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+						
+			// create a course
+			Course javaCourse = new Course("Perscholas -Amex Java FullStack ");
+						
+			// save the course
+			System.out.println("\nSaving the course ...");
+			session.save(javaCourse);
+			System.out.println("Saved the course: " + javaCourse);
+			
+			// create one java students
+			Student javaFrehiwot = new Student("Frehiwot", "Meron", "fmeron@gmail.com");
+			
+						
+			// add students to the course
+			javaCourse.addStudent(javaFrehiwot);
+			
+			
+			// save the students
+			System.out.println("\nSaving students ...");
+			session.save(javaFrehiwot);
+			
+			System.out.println("Saved students: " + javaCourse.getStudents());
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
---
+++ SBA (2) (1)/SBA/java-hibernate-sba/src/com/mycapstone/hibernate/demo/SbaQ5_getStudentID.java
@@ -0,0 +1,72 @@
+package com.mycapstone.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.mycapstone.hibernate.demo.entity.Course;
+import com.mycapstone.hibernate.demo.entity.Instructor;
+import com.mycapstone.hibernate.demo.entity.InstructorDetail;
+import com.mycapstone.hibernate.demo.entity.Review;
+import com.mycapstone.hibernate.demo.entity.Student;
+
+public class SbaQ5_getStudentID {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Instructor.class)
+								.addAnnotatedClass(InstructorDetail.class)
+								.addAnnotatedClass(Course.class)
+								.addAnnotatedClass(Review.class)
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// start a transaction
+			session.beginTransaction();
+				
+			// get the student mary from database
+			int studentId = 1;
+			Student StudentFrehiwort = session.get(Student.class, studentId);
+			
+			System.out.println("\nLoaded student: " + StudentFrehiwort);
+			System.out.println("Courses: " + StudentFrehiwort.getCourses());
+			
+			// create more courses 
+			
+						
+			// add student to courses
+			
+						
+			// save the courses
+			
+			
+			
+						
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			
+			// add clean up code
+			session.close();
+			
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
